0ff3fa4589cb3ec5e382373d3d0782c8ddd3a473
rohitv.gupta <rohitv.gupta@Impetus-N406.impetus.co.in>
2016-11-07 13:43:34 +0530

    Inital checkin

---
+++ src/poc/microservices/instance/two/controller/InstanceTwoController.java
@@ -0,0 +1,38 @@
+package poc.microservices.instance.two.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import poc.microservices.instance.two.dto.User;
+
+@RestController
+public class InstanceTwoController {
+
+	private Map<String, Integer> totalRequestCountMap;
+
+	public InstanceTwoController() {
+		HazelcastInstance instance = Hazelcast.newHazelcastInstance();
+		totalRequestCountMap = instance.getMap("requestCount");
+	}
+
+	@RequestMapping("/getUser")
+	public User test(
+			@RequestParam(value = "name", defaultValue = "Instance2") String name) {
+		System.out.println("22222222222222222222222222222");
+		Integer count = totalRequestCountMap.get("user");
+		if (count == null) {
+			totalRequestCountMap.put("user", 0);
+		} else {
+			totalRequestCountMap.put("user", count + 1);
+		}
+//		System.out.println("Total nos. of rwquest :"
+//				+ totalRequestCountMap.get("user"));
+		return new User(22222, name);
+	}
+}
